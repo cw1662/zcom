04bd537d124f9c0d9e2933c150dd78798600fa48
cw1662 <chaewon1662@naver.com>
2024-03-07 20:52:55 +0900

    update1

---
+++ z-com/src/app/(afterLogin)/[username]/page.tsx
@@ -0,0 +1,11 @@
+/* user의 페이지 */
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Profile() {
+  return (
+    <main>
+      
+    </main>
+  );
+}
---
+++ z-com/src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,14 @@
+export default async function HomeLayout({
+   children,
+ }: Readonly<{
+   children: React.ReactNode;
+ }>) {
+ return (
+    <div>
+      홈 레이아웃
+      {children}
+      </div>
+ );   
+}
+
+// RootLayout -> HomeLayout -> Home
